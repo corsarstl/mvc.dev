b71536d6a65eac7643dde5ca9f36429e8baefbed
Vitalii Syromiatnykov <corsarstl@gmail.com>
2017-07-03 17:57:31 +0300

    url parcing, Router class

--- webroot/index.php
+++ webroot/index.php
@@ -10,3 +10,11 @@ $router = new Router($_SERVER['REQUEST_URI']);
 //$uri = $_SERVER['REQUEST_URI'];
 //
 //print_r($uri);
+
+echo "<pre>";
+print_r('Route: '.$router->getRoute().PHP_EOL);
+print_r('Language: '.$router->getLanguage().PHP_EOL);
+print_r('Controller: '.$router->getController().PHP_EOL);
+print_r('Action to be called: '.$router->getMethodPrefix().$router->getAction().PHP_EOL);
+echo 'Params: ';
+print_r($router->getParams());
