38f865a0633ea494310a9aee70e27c45abb5a97c
Vitalii Syromiatnykov <corsarstl@gmail.com>
2017-07-04 20:04:24 +0300

    Controllers and views

--- lib/init.php
+++ lib/init.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Виталий
- * Date: 16.06.2017
- * Time: 17:22
- */
 
 require_once (ROOT.DS.'config'.DS.'config.php');
 
--- webroot/index.php
+++ webroot/index.php
@@ -2,6 +2,7 @@
 
 define ('DS', DIRECTORY_SEPARATOR);
 define ('ROOT', dirname(dirname(__FILE__)));
+define ('VIEWS_PATH', ROOT.DS.'views');
 
 require_once (ROOT.DS.'lib'.DS.'init.php');
 
@@ -11,10 +12,12 @@ $router = new Router($_SERVER['REQUEST_URI']);
 //
 //print_r($uri);
 
-echo "<pre>";
-print_r('Route: '.$router->getRoute().PHP_EOL);
-print_r('Language: '.$router->getLanguage().PHP_EOL);
-print_r('Controller: '.$router->getController().PHP_EOL);
-print_r('Action to be called: '.$router->getMethodPrefix().$router->getAction().PHP_EOL);
-echo 'Params: ';
-print_r($router->getParams());
+//echo "<pre>";
+//print_r('Route: '.$router->getRoute().PHP_EOL);
+//print_r('Language: '.$router->getLanguage().PHP_EOL);
+//print_r('Controller: '.$router->getController().PHP_EOL);
+//print_r('Action to be called: '.$router->getMethodPrefix().$router->getAction().PHP_EOL);
+//echo 'Params: ';
+//print_r($router->getParams());
+
+App::run($_SERVER['REQUEST_URI']);
